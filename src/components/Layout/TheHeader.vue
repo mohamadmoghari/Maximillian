e8e40102798ce72af8630ab0413656d864c53530
Mohammad Moghari <mohamadmoghari@hotmail.com>
2022-03-22 01:04:55 +0430

    Add TheHeader

---
+++ src/components/Layout/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  min-height: 5rem;
+  background-color: brown;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+header h1 {
+  color: white;
+}
+</style>
