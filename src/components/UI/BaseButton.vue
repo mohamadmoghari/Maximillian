2d3d59bd7e2b6a4cacd1fecba3442e62359a3d63
Mohammad Moghari <mohamadmoghari@hotmail.com>
2022-03-22 01:04:35 +0430

    Add BaseButton

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button :type="type" :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required:true
+    },
+    mode: {
+      type: String,
+      required:true
+    },
+  }, //['type', 'mode'],
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  cursor: pointer;
+  background-color: indigo;
+  color: white;
+  text-align: center;
+}
+
+.flat {
+  padding: 0.75rem 1.5rem;
+  cursor: pointer;
+  background-color: inherit;
+  outline: none;
+  color: black;
+  border:none;
+}
+.flat:hover {
+  color: white;
+  cursor: pointer;
+  background-color: blueviolet;
+  border-color: blueviolet;
+  opacity: 0.5;
+}
+</style>
